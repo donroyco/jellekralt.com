25e33b5a98aabf2b6fd82e784a5e0770176f0a6a
Jelle Kralt <jelle@jellekralt.nl>
2017-10-01 08:53:41 +0200

    Changes to about

--- components/About.vue
+++ components/About.vue
@@ -1,16 +1,16 @@
 <template>
   <section id="about">
     <p>
-      Hi, I'm Jelle Kralt. Front End Engineer at CODEZILLA. I ❤️ JavaScript, Angular and Node.js
+      Hi, I'm Jelle Kralt. Front End Engineer at CODEZILLA. I ❤️ JavaScript.
     </p>
     <p>
       <span>Find me on </span>
       <span><a class="icon" target="_blank" href="https://github.com/jellekralt" title="Github"><i class="fa fa-github"></i></a></span>,
       <span><a class="icon" target="_blank" href="https://twitter.com/jellekralt" title="Twitter"><i class="fa fa-twitter"></i></a></span>,
-      <span><a class="icon" target="_blank" href="https://keybase.io/jellekralt/" title="Keybase"><i class="fa fa-key"></i></a></span>
+      <span><a class="icon" target="_blank" href="http://nl.linkedin.com/in/jellekralt" title="LinkedIn"><i class="fa fa-linkedin"></i></a></span>,
       and 
-      <span><a class="icon" target="_blank" href="http://nl.linkedin.com/in/jellekralt" title="LinkedIn"><i class="fa fa-linkedin"></i></a></span>
+      <span><a class="icon" target="_blank" href="https://keybase.io/jellekralt/" title="Keybase"><i class="fa fa-key"></i></a></span>
     </p>
-    <p>This site is currently under construction...</p>
+    <p>This page is currently under construction...</p>
   </section>
 </template>

e3411d014136a2e7aa29dadcf4150bc1dbc994a7
Jelle Kralt <jelle@jellekralt.nl>
2017-10-15 22:02:23 +0200

    fix: improvements from chrome audit

--- components/About.vue
+++ components/About.vue
@@ -5,11 +5,11 @@
     </p>
     <p>
       <span>Find me on </span>
-      <span><a class="icon" target="_blank" href="https://github.com/jellekralt" title="Github"><i class="fa fa-github"></i></a></span>,
-      <span><a class="icon" target="_blank" href="https://twitter.com/jellekralt" title="Twitter"><i class="fa fa-twitter"></i></a></span>,
-      <span><a class="icon" target="_blank" href="http://nl.linkedin.com/in/jellekralt" title="LinkedIn"><i class="fa fa-linkedin"></i></a></span>,
+      <span><a class="icon" target="_blank" rel="noopener" href="https://github.com/jellekralt" title="Github"><i class="fa fa-github"></i></a></span>,
+      <span><a class="icon" target="_blank" rel="noopener" href="https://twitter.com/jellekralt" title="Twitter"><i class="fa fa-twitter"></i></a></span>,
+      <span><a class="icon" target="_blank" rel="noopener" href="http://nl.linkedin.com/in/jellekralt" title="LinkedIn"><i class="fa fa-linkedin"></i></a></span>,
       and 
-      <span><a class="icon" target="_blank" href="https://keybase.io/jellekralt/" title="Keybase"><i class="fa fa-key"></i></a></span>
+      <span><a class="icon" target="_blank" rel="noopener" href="https://keybase.io/jellekralt/" title="Keybase"><i class="fa fa-key"></i></a></span>
     </p>
   </section>
 </template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,10 +3,13 @@ module.exports = {
   ** Headers of the page
   */
   head: {
+    htmlAttrs: {
+      lang: 'en'
+    },
     title: 'Jelle Kralt',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=5' },
       { hid: 'description', name: 'description', content: 'Hi, I\'m Jelle Kralt. Front End Engineer at CODEZILLA. I ❤️ JavaScript' },
       { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' }
     ],

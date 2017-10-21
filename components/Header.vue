4e4cd0cf102a5703fa6b73473a908770961d72b3
Jelle Kralt <jelle@jellekralt.nl>
2017-10-21 22:13:42 +0200

    feature: Add header roles

--- components/Header.vue
+++ components/Header.vue
@@ -1,21 +1,19 @@
 <template>
-  <header id="header">
+  <header id="header" role="header">
     <a href="/">
-  
-      <div id="logo"><img src="~/assets/img/jk.svg" alt="Jelle Kralt" /></div>
-    
+      <div id="logo"><img src="~/assets/img/jk.svg" alt="Logo" /></div>
       <div id="title">
         <h1>Jelle Kralt</h1>
       </div>
     </a>
-    <div id="nav">
+    <nav id="nav" role="navigation">
       <ul :class="{ 'open': menuIsOpen }">
         <li class="icon" @click="menuIsOpen = !menuIsOpen">
           <button title="Menu"><i class="fa fa-bars fa-2x"></i></button>
         </li>        
         <li v-for="link in navigation" v-bind:key="link.href"><a :href="link.href">{{ link.title }}</a></li>
       </ul>
-    </div>
+    </nav>
   </header>
 
 </template>

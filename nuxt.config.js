7c7dc85a591036dcece4fccac2fe9d2423aee8ec
Jelle Kralt <jelle@jellekralt.nl>
2018-03-02 10:10:17 +0100

    Temporarily disabled eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,14 +73,14 @@ module.exports = {
     ** Run ESLINT on save
     */
     extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        });
-      }
+      // if (ctx.isClient) {
+      //   config.module.rules.push({
+      //     enforce: 'pre',
+      //     test: /\.(js|vue)$/,
+      //     loader: 'eslint-loader',
+      //     exclude: /(node_modules)/
+      //   });
+      // }
 
       config.module.rules.forEach((rule) => {
         if (isVueRule(rule)) {
